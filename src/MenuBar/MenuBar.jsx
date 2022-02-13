f6b3943d247870166d82a3b41e45538709c33753
Luiz Antonio <80444739+luizantoniofx@users.noreply.github.com>
2022-02-13 03:21:11 -0300

    Changed some menu items and added react-tsparticles for background.

--- src/MenuBar/MenuBar.jsx
+++ src/MenuBar/MenuBar.jsx
@@ -10,7 +10,7 @@ export default () => {
     <div classname="menulist">
         <ul>
             <li><a href=''>Home</a></li>
-            <li><a href='http://storage.ibukicloud.tk:90/'>Storage</a></li>
+            <li><a href='http://storage.ibukicloud.tk:90/'>Cloud Storage</a></li>
             <li><a href=''>Minecraft Servers</a></li>
             <li><a href='https://github.com/luizantoniofx/ibukicloud-documentation'>Server Documentation</a></li>
             <li><a href='http://blog.ibukicloud.tk:90/'>Blog and Updates</a></li>
--- src/app.jsx
+++ src/app.jsx
@@ -4,10 +4,16 @@ import MenuBar from './MenuBar/MenuBar'
 import BodyDefault from './BodyDefault/BodyDefault'
 import ServerStatus from './ServerStatus/ServerStatus'
 import Footer from './Footer/Footer'
+import Particles from "react-tsparticles";
 
 const App = () => {
     return (
         <div className='container-default'>
+        
+        <div className='ParticlesD'>
+            <ParticlesV/>
+        </div>
+
             <div className="menu">
                 <MenuBar/>
             </div>
@@ -15,13 +21,106 @@ const App = () => {
         <main className="defaultpg">
             <BodyDefault/>
         </main>
-        <div className='servers'>
-            <ServerStatus/>
-        </div>
-        <div className='footer'>
-            <Footer/>
-        </div>
+
+            <div className='servers'>
+                <ServerStatus/>
+            </div>
+
+            <div className='footer'>
+                <Footer/>
+            </div>
     </div>
     )
 }
+
+const ParticlesV = () => {
+    const particlesInit = (main) => {
+        console.log(main);
+    
+        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+      };
+    
+      const particlesLoaded = (container) => {
+        console.log(container);
+      };
+      return (
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: false,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: false,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                bubble: {
+                  distance: 400,
+                  duration: 2,
+                  opacity: 0.8,
+                  size: 40,
+                },
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#FF2D00",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: false,
+                opacity: 0.5,
+                width: 1,
+              },
+              collisions: {
+                enable: false,
+              },
+              move: {
+                direction: "left",
+                enable: true,
+                outMode: "out",
+                random: false,
+                speed: 8,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 500,
+                },
+                value: 90,
+              },
+              opacity: {
+                value: 0.2,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                random: true,
+                value: 4,
+              },
+            },
+            detectRetina: false,
+          }}
+        />
+      );
+  };
 export default App
